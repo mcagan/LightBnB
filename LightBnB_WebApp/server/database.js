e9cd59fa0ed3abae4ac1cb8799ee3ae5269ace35
mcagan <meliscagan@gmail.com>
2020-04-02 16:08:50 +0000

    impelemnt getReservations query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -64,7 +64,11 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryText = "SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating FROM properties JOIN reservations ON property_id = properties.id JOIN property_reviews ON property_reviews.property_id = properties.id WHERE reservations.guest_id = $1 AND end_date < now()::date GROUP BY properties.id, reservations.id ORDER BY reservations.start_date LIMIT $2;"
+  const values = [guest_id, limit]
+  return pool.query(queryText, values)
+  .then(res => res.rows)
+  .catch(err => console.log('Err', err));
 }
 exports.getAllReservations = getAllReservations;
 
